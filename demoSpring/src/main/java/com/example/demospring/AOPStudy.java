84ba3d40c5374407cdfb053dce45debdafac2823
lixiaochong <1379989321@qq.com>
2022-11-02 21:16:01 +0800

    sss

---
+++ demoSpring/src/main/java/com/example/demospring/AOPStudy.java
@@ -0,0 +1,10 @@
+package com.example.demospring;
+
+import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator;
+
+public class AOPStudy {
+
+
+    AnnotationAwareAspectJAutoProxyCreator creator = new AnnotationAwareAspectJAutoProxyCreator();
+
+}
---
+++ demoSpring/src/main/java/com/example/demospring/aop/LogAspects.java
@@ -0,0 +1,4 @@
+package com.example.demospring.aop;
+
+public class LogAspects {
+}
